d74511672fd110228651dc65f38be1cdced93476
Utsav Gupta <utgupta27@gmail.com>
2021-10-24 17:55:03 +0530

    ready to release build

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,16 @@ import 'package:currency_market/home_page.dart';
 import 'package:flutter/material.dart';
 
 main(List<String> args) {
-  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Currency Market",
       home: HomePage(),
     );
